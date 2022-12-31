e0ad1eb367aaf15f5cade39164ddc170fb433ca2
vishalvijayakumarv <vishalvijayakumarv@gmail.com>
2022-12-31 13:13:07 +0530

    updated sh file

--- jenkins_trigger.sh
+++ jenkins_trigger.sh
@@ -10,6 +10,7 @@ CRUMB_URL="$JENKINS_HOME_URL/crumbIssuer/api/xml?xpath=$CRUMB_XPATH"
 JOB_URL="$JENKINS_HOME_URL/job/$JOB_NAME/build?delay=0sec"
 CRUMB_JSON_URL="$(curl -s -X GET $JENKINS_HOME_URL/crumbIssuer/api/json --user $JENKINS_USER:$JENKINS_PASSWORD)"
 CRUMB="$(echo $CRUMB_JSON_URL | jq -r '.crumb' )"
+
 echo $CRUMB
 echo $JENKINS_USER
 echo $JENKINS_PASSWORD
